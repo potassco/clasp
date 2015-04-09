d4282134bd1ab5de2686ec6fd1c48caf2ecb0607
Benjamin Kaufmann <kaufmann@cs.uni-potsdam.de>
2015-04-09 13:22:31 +0000

    * too strong assertion
    git-svn-id: https://svn.cs.uni-potsdam.de/svn/reposWV/Privat/BenjaminKaufmann/branches/clasp-3.1.x@45852 adcd87e9-38e8-0310-969d-ff0ec0a098a6

--- libclasp/src/logic_program_types.cpp
+++ libclasp/src/logic_program_types.cpp
@@ -1129,7 +1129,11 @@ bool PrgBody::mergeHeads(LogicProgram& prg, PrgBody& heads, bool strong, bool si
 		else {
 			assert(heads.sHead_ == 0 && "Heads to merge not simplified!");
 			heads.prepareSimplifyHeads(prg, rs);
-			ok = simplifyHeadsImpl(prg, *this, rs, strong) && heads.simplifyHeadsImpl(prg, *this, rs, strong);
+			if (!simplifyHeadsImpl(prg, *this, rs, strong) && !assignValue(value_false)) {
+				rs.clearAll();
+				return false;
+			}
+			ok = heads.simplifyHeadsImpl(prg, *this, rs, strong);
 			assert(ok || heads.heads_begin() == heads.heads_end());
 		}
 		// clear temporary flags & reestablish ordering
