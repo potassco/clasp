4c3c4ab160daea396982c07045d0653865b7a34d
Benjamin Kaufmann <kaufmann@cs.uni-potsdam.de>
2016-09-21 15:25:32 +0200

    Fix invalid name in cmake config

--- libpotassco/cmake/potassco-config.cmake
+++ libpotassco/cmake/potassco-config.cmake
@@ -3,5 +3,5 @@
 # https://foonathan.github.io/blog/2016/03/03/cmake-install.html
 
 get_filename_component(SELF_DIR "${CMAKE_CURRENT_LIST_FILE}" PATH)
-include(${SELF_DIR}/${CMAKE_BUILD_TYPE}/potassco_lp.cmake)
+include(${SELF_DIR}/${CMAKE_BUILD_TYPE}/potassco.cmake)
 
