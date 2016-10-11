b706477bf6a83972606f98c4d4e0c91e7f8dcbd6
Benjamin Kaufmann <kaufmann@cs.uni-potsdam.de>
2016-10-11 10:37:38 +0200

    Modernize cmake build files

---
+++ cmake/clasp-config.cmake
@@ -0,0 +1,7 @@
+# clasp-config.cmake - package configuration file
+# Note: Adapted from Jonathan Müller's blog post on supporting CMake install:
+# https://foonathan.github.io/blog/2016/03/03/cmake-install.html
+
+get_filename_component(SELF_DIR "${CMAKE_CURRENT_LIST_FILE}" PATH)
+include(${SELF_DIR}/${CMAKE_BUILD_TYPE}/clasp.cmake)
+
