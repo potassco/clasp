eb0d8d178e19c9beb788f55d2b66f6c0bb240a14
Benjamin Kaufmann <kaufmann@cs.uni-potsdam.de>
2016-11-27 17:58:25 +0100

    Add abort() to fail macros to disable warnings

--- libpotassco/potassco/platform.h
+++ libpotassco/potassco/platform.h
@@ -77,10 +77,10 @@ extern void fail(const char* function, unsigned line, int type, const char* fmt,
 } // namespace Potassco
 
 #define POTASSCO_FAIL_IF(exp, ...) \
-	(void)( (!(exp)) || (Potassco::fail(POTASSCO_FUNC_NAME, __LINE__, 0, __VA_ARGS__), 0))
+	(void)( (!(exp)) || (Potassco::fail(POTASSCO_FUNC_NAME, __LINE__, 0, __VA_ARGS__), std::abort(), 0))
 
 #define POTASSCO_ASSERT_CONTRACT_MSG(exp, ...) \
-	(void)( (!!(exp)) || (Potassco::fail(POTASSCO_FUNC_NAME, __LINE__, 1, __VA_ARGS__), 0))
+	(void)( (!!(exp)) || (Potassco::fail(POTASSCO_FUNC_NAME, __LINE__, 1, __VA_ARGS__), std::abort(), 0))
 
 #define POTASSCO_ASSERT_CONTRACT(exp) POTASSCO_ASSERT_CONTRACT_MSG(exp, #exp)
 
