potassco/clasp

commit 7335240bd2e0fe508f6c1273ca329a083cc60bd6

Defer detaching of solver(s) until SolveAlgorithm has finished.
* Only detach solver(s) after the active solve algorithm has computed
  a final unsat core (if any) so that aux variables are still valid.

src/solve_algorithms.cpp
@@ -283,24 +283,31 @@ bool SolveAlgorithm::attach(SharedContext& ctx, ModelHandler* onModel) {
 void SolveAlgorithm::detach() {
 	if (ctx_) {
 		if (enum_->enumerated() == 0 && !interrupted()) {
-			uint32  w = ctx_->winner();
-			Solver* s = ctx_->hasSolver(w) ? ctx_->solver(w) : ctx_->solver(0);
+			Solver* s = ctx_->master();
+			Literal step = ctx_->stepLiteral();
 			s->popRootLevel(s->rootLevel());
 			core_ = new LitVec();
 			for (LitVec::const_iterator it = path_->begin(); it != path_->end(); ++it) {
-				if (s->isTrue(*it) || *it == ctx_->stepLiteral())
+				if (s->isTrue(*it) || *it == step)
 					continue;
+				if (!s->isTrue(step) && !s->pushRoot(step))
+					break;
 				core_->push_back(*it);
 				if (!s->pushRoot(*it)) {
 					if (!s->isFalse(*it)) {
 						core_->clear();
 						s->resolveToCore(*core_);
+						if (!core_->empty() && (*core_)[0] == step) {
+							core_->front() = core_->back();
+							core_->pop_back();
+						}
 					}
 					break;
 				}
 			}
 			s->popRootLevel(s->rootLevel());
 		}
+		doDetach();
 		ctx_->master()->stats.addCpuTime(ThreadTime::getTime() - time_);
 		onModel_ = 0;
 		ctx_     = 0;
@@ -437,19 +444,22 @@ int SequentialSolve::doNext(int last) {
 void SequentialSolve::doStop() {
 	if (solve_.get()) {
 		enumerator().end(solve_->solver());
-		ctx().detach(solve_->solver());
-		solve_ = 0;
 	}
 }
+void SequentialSolve::doDetach() {
+	ctx().detach(solve_->solver());
+	solve_ = 0;
+}
+
 bool SequentialSolve::doSolve(SharedContext& ctx, const LitVec& gp) {
-	BasicSolve solve(*ctx.master(), ctx.configuration()->search(0), limits());
+	solve_.reset(new BasicSolve(*ctx.master(), ctx.configuration()->search(0), limits()));
 	// Add assumptions - if this fails, the problem is unsat
 	// under the current assumptions but not necessarily unsat.
-	Solver& s = solve.solver();
+	Solver& s = solve_->solver();
 	bool more = !interrupted() && ctx.attach(s) && enumerator().start(s, gp);
 	for (InterruptHandler term(term_ >= 0 ? &s : (Solver*)0, &term_); more;) {
 		ValueRep res;
-		while ((res = solve.solve()) == value_true && (!enumerator().commitModel(s) || reportModel(s))) {
+		while ((res = solve_->solve()) == value_true && (!enumerator().commitModel(s) || reportModel(s))) {
 			enumerator().update(s);
 		}
 		if      (res != value_false)           { more = (res == value_free || moreModels(s)); break; }
@@ -459,7 +469,6 @@ bool SequentialSolve::doSolve(SharedContext& ctx, const LitVec& gp) {
 		else                                   { enumerator().end(s); more = enumerator().start(s, gp); }
 	}
 	enumerator().end(s);
-	ctx.detach(s);
 	return more;
 }
 }

clasp/mt/parallel_solve.h
@@ -149,7 +149,7 @@ private:
 	struct EntryPoint;
 	void   destroyThread(uint32 id);
 	void   allocThread(uint32 id, Solver& s);
-	void   joinThreads();
+	int    joinThreads();
 	// -------------------------------------------------------------------------------------------
 	// Algorithm steps
 	void   setIntegrate(uint32 grace, uint8 filter);
@@ -159,6 +159,7 @@ private:
 	void   doStart(SharedContext& ctx, const LitVec& assume);
 	int    doNext(int last);
 	void   doStop();
+	void   doDetach();
 	bool   doInterrupt();
 	void   solveParallel(uint32 id);
 	void   initQueue();

clasp/solve_algorithms.h
@@ -224,6 +224,7 @@ protected:
 	virtual void    doStart(SharedContext& ctx, const LitVec& assume);
 	virtual int     doNext(int last);
 	virtual void    doStop();
+	virtual void    doDetach() = 0;
 
 	bool            reportModel(Solver& s) const;
 	bool            reportUnsat(Solver& s) const;
@@ -263,6 +264,7 @@ protected:
 	virtual void doStart(SharedContext& ctx, const LitVec& assume);
 	virtual int  doNext(int last);
 	virtual void doStop();
+	virtual void doDetach();
 private:
 	typedef SingleOwnerPtr<BasicSolve> SolvePtr;
 	SolvePtr     solve_;

src/parallel_solve.cpp
@@ -418,8 +418,9 @@ inline void ParallelSolve::reportProgress(const Solver& s, const char* msg) cons
 }
 
 // joins with and destroys all active threads
-void ParallelSolve::joinThreads() {
+int ParallelSolve::joinThreads() {
 	uint32 winner = thread_[masterId]->winner() ? uint32(masterId) : UINT32_MAX;
+	// detach master only after all client threads are done
 	for (uint32 i = 1, end = shared_->nextId; i != end; ++i) {
 		thread_[i]->join();
 		if (thread_[i]->winner() && i < winner) {
@@ -433,13 +434,12 @@ void ParallelSolve::joinThreads() {
 	if (shared_->complete()) {
 		enumerator().commitComplete();
 	}
-	// detach master only after all client threads are done
-	thread_[masterId]->detach(*shared_->ctx, shared_->interrupt());
-	thread_[masterId]->setError(!shared_->interrupt() ? thread_[masterId]->error() : shared_->errorCode);
+	thread_[masterId]->handleTerminateMessage();
 	shared_->ctx->setWinner(winner);
 	shared_->nextId = 1;
 	shared_->syncT.stop();
 	reportProgress(MessageEvent(*shared_->ctx->master(), "TERMINATE", MessageEvent::completed, shared_->syncT.total()));
+	return !shared_->interrupt() ? thread_[masterId]->error() : shared_->errorCode;
 }
 
 void ParallelSolve::doStart(SharedContext& ctx, const LitVec& assume) {
@@ -469,9 +469,7 @@ void ParallelSolve::doStop() {
 		shared_->generator->notify(SharedData::Generator::done);
 		thread_[masterId]->join();
 	}
-	joinThreads();
-	int err = thread_[masterId]->error();
-	destroyThread(masterId);
+	int err = joinThreads();
 	shared_->generator = 0;
 	shared_->ctx->distributor.reset(0);
 	switch(err) {
@@ -481,6 +479,12 @@ void ParallelSolve::doStop() {
 		case OutOfMemory:  throw std::bad_alloc();
 		default:           throw std::runtime_error(shared_->msg.c_str());
 	}
+}
+
+void ParallelSolve::doDetach() {
+	// detach master only after all client threads are done
+	thread_[masterId]->detach(*shared_->ctx, shared_->interrupt());
+	destroyThread(masterId);
 }
 
 // Entry point for master solver

commit 4104dd2d84d558433e135c2f1bffd7eb7eb932ab

Fix issue #72: Regression introduced in 7335240b.
* Fix regression in iterative (sequential) solving where UNSAT detected
  in start() no longer terminated the algorithm in the following call
  to next().

src/solve_algorithms.cpp
@@ -444,22 +444,22 @@ int SequentialSolve::doNext(int last) {
 void SequentialSolve::doStop() {
 	if (solve_.get()) {
 		enumerator().end(solve_->solver());
+		solve_ = 0;
 	}
 }
 void SequentialSolve::doDetach() {
-	ctx().detach(solve_->solver());
-	solve_ = 0;
+	ctx().detach(*ctx().master());
 }
 
 bool SequentialSolve::doSolve(SharedContext& ctx, const LitVec& gp) {
-	solve_.reset(new BasicSolve(*ctx.master(), ctx.configuration()->search(0), limits()));
 	// Add assumptions - if this fails, the problem is unsat
 	// under the current assumptions but not necessarily unsat.
-	Solver& s = solve_->solver();
+	Solver& s = *ctx.master();
 	bool more = !interrupted() && ctx.attach(s) && enumerator().start(s, gp);
-	for (InterruptHandler term(term_ >= 0 ? &s : (Solver*)0, &term_); more;) {
+	InterruptHandler term(term_ >= 0 ? &s : (Solver*)0, &term_);
+	for (BasicSolve solve(s, ctx.configuration()->search(0), limits()); more;) {
 		ValueRep res;
-		while ((res = solve_->solve()) == value_true && (!enumerator().commitModel(s) || reportModel(s))) {
+		while ((res = solve.solve()) == value_true && (!enumerator().commitModel(s) || reportModel(s))) {
 			enumerator().update(s);
 		}
 		if      (res != value_false)           { more = (res == value_free || moreModels(s)); break; }

tests/facade_test.cpp
@@ -621,6 +621,12 @@ TEST_CASE("Facade", "[facade]") {
 			REQUIRE(exp == got);
 		}
 	}
+	SECTION("testGenSolveStartUnsat") {
+		lpAdd(libclasp.startAsp(config, true), "{x1, x2}.\n :- x1, x2.\n#assume{x1, x2}.");
+		libclasp.prepare();
+		ClaspFacade::SolveHandle it = libclasp.solve(SolveMode_t::Yield);
+		REQUIRE_FALSE(it.next());
+	}
 
 	SECTION("testCancelGenSolve") {
 		config.solve.numModels = 0;