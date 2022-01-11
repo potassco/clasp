potassco/clasp

commit e3cf296b8a9f74d29b2ac7e23bde6c693bdb759c

Enumerator: Push step literal first
* Push step literal first when adding current path so that it is
  already assigned before propagators can add volatile clauses over
  initial assumptions.

src/enumerator.cpp
@@ -94,7 +94,7 @@ bool EnumerationConstraint::start(Solver& s, const LitVec& path, bool disjoint) 
 	state_ = 0;
 	root_  = s.rootLevel();
 	setDisjoint(disjoint);
-	if (s.pushRoot(path) && s.pushRoot(s.sharedContext()->stepLiteral())) {
+	if (s.pushRoot(s.sharedContext()->stepLiteral()) && s.pushRoot(path)) {
 		integrateBound(s);
 		integrateNogoods(s);
 		return true;

src/clingo.cpp
@@ -300,6 +300,7 @@ void ClingoPropagator::toClause(Solver& s, const Potassco::LitSpan& clause, Pota
 	if (aux_ < max) { aux_ = max; }
 	if ((Potassco::Clause_t::isVolatile(prop) || s.auxVar(max.var())) && !isSentinel(s.sharedContext()->stepLiteral())) {
 		mem.push_back(~s.sharedContext()->stepLiteral());
+		POTASSCO_REQUIRE(s.value(mem.back().var()) != value_free || s.decisionLevel() == 0, "Step literal must be assigned on level 1");
 	}
 	todo_.clause = ClauseCreator::prepare(s, mem, Clasp::ClauseCreator::clause_force_simplify, Constraint_t::Other);
 	todo_.flags  = ccFlags_s[int(Potassco::Clause_t::isStatic(prop))];

commit 91e1da92fc95469c34b078e1ae6f4bac9a97d361

pushRoot() only on fully propagated root level.
* This fixes a regression introduced in commit e3cf296
* See https://github.com/potassco/clingcon/issues/85

clasp/solver.h
@@ -146,7 +146,7 @@ public:
 	void                    removePost(PostPropagator* p);
 
 	//! Adds path to the current root-path and adjusts the root-level accordingly.
-	bool pushRoot(const LitVec& path);
+	bool pushRoot(const LitVec& path, bool pushStep = false);
 	bool pushRoot(Literal p);
 	void setEnumerationConstraint(Constraint* c);
 	//! Requests a special aux variable for tagging conditional knowledge.

src/enumerator.cpp
@@ -94,7 +94,7 @@ bool EnumerationConstraint::start(Solver& s, const LitVec& path, bool disjoint) 
 	state_ = 0;
 	root_  = s.rootLevel();
 	setDisjoint(disjoint);
-	if (s.pushRoot(s.sharedContext()->stepLiteral()) && s.pushRoot(path)) {
+	if (s.pushRoot(path, true)) {
 		integrateBound(s);
 		integrateNogoods(s);
 		return true;

src/solver.cpp
@@ -464,10 +464,11 @@ Literal Solver::popVars(uint32 num, bool popLearnt, ConstraintDB* popAux) {
 	return pop;
 }
 
-bool Solver::pushRoot(const LitVec& path) {
-	// make sure we are on the current root level
+bool Solver::pushRoot(const LitVec& path, bool pushStep) {
+	// make sure we are on the current (fully propagated) root level
 	if (!popRootLevel(0) || !simplify() || !propagate()) { return false; }
 	// push path
+	if (pushStep && !pushRoot(shared_->stepLiteral())) { return false; }
 	stats.addPath(path.size());
 	for (LitVec::const_iterator it = path.begin(), end = path.end(); it != end; ++it) {
 		if (!pushRoot(*it)) { return false; }